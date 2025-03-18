1c00ad20ef2ecdf4e935f6f4b6508bfefbb1ce76
TutorialWithAI <96469380+OrlandoFabrizio@users.noreply.github.com>
2025-03-18 08:55:26 +0000

    test

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -57,7 +57,7 @@ const medusaConfig = {
               endPoint: MINIO_ENDPOINT,
               accessKey: MINIO_ACCESS_KEY,
               secretKey: MINIO_SECRET_KEY,
-              bucket: MINIO_BUCKET // Optional, default: medusa-media
+              bucket: MINIO_BUCKET
             }
           }] : [{
             resolve: '@medusajs/file-local',
@@ -130,7 +130,7 @@ const medusaConfig = {
     }] : [])
   ],
   plugins: [
-  ...(MEILISEARCH_HOST && MEILISEARCH_ADMIN_KEY ? [{
+    ...(MEILISEARCH_HOST && MEILISEARCH_ADMIN_KEY ? [{
       resolve: '@rokmohar/medusa-plugin-meilisearch',
       options: {
         config: {
@@ -147,7 +147,13 @@ const medusaConfig = {
           }
         }
       }
-    }] : [])
+    }] : []),
+    {
+      resolve: 'medusa-plugin-blogger',
+      options: {
+        enableUI: true,
+      }
+    }
   ]
 };
 
